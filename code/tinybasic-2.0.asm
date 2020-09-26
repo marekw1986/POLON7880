cfef6842c2a6e6b744e0df4d95cfda0759437f05
Marek Wiecek <marekw1986@gmail.com>
2020-09-27 00:17:01 +0200

    some changes

--- code/tinybasic-2.0.asm
+++ code/tinybasic-2.0.asm
@@ -1972,25 +1972,25 @@ INIT:   STA  OCSW
         MVI	 A, 27H
         OUT	 UART_8251_CTRL
         ;Initialize 8259
-        MVI  A, 0FFH					;ICW1 - LSB of IR0_VECT = 0xE0, level triggered, 4 byte intervals, one 8259, ICW4 needed
-        OUT  PIC_8259_LOW				;ICW1 is written to the low port of 8259
-        MVI  A, 0FFH					;ICW2, MSB of IR0_VECT
-        OUT	 PIC_8259_HIGH				;ICW2 is written to the high port of 8259
-        MVI  A, 02H						;ICW4 - NOT special full nested mode, not buffored, master, automatic EOI, 8080 processor
-        OUT  PIC_8259_HIGH				;ICW4 is written to the high port of 8259        
-        MVI  A, 9BH						;OCW1 active TIMER, RTC and KBD interrupt
-        OUT  PIC_8259_HIGH				;OCW1 is written to the high port of 8259
-        MVI  A, 80H						;OCW2 - Rotation of priorities, no explicit EOI
-        OUT  PIC_8259_LOW				;OCW2 is written to the low port of 8259
+        ;MVI  A, 0FFH					;ICW1 - LSB of IR0_VECT = 0xE0, level triggered, 4 byte intervals, one 8259, ICW4 needed
+        ;OUT  PIC_8259_LOW				;ICW1 is written to the low port of 8259
+        ;MVI  A, 0FFH					;ICW2, MSB of IR0_VECT
+        ;OUT	 PIC_8259_HIGH				;ICW2 is written to the high port of 8259
+        ;MVI  A, 02H						;ICW4 - NOT special full nested mode, not buffored, master, automatic EOI, 8080 processor
+        ;OUT  PIC_8259_HIGH				;ICW4 is written to the high port of 8259        
+        ;MVI  A, 9BH						;OCW1 active TIMER, RTC and KBD interrupt
+        ;OUT  PIC_8259_HIGH				;OCW1 is written to the high port of 8259
+        ;MVI  A, 80H						;OCW2 - Rotation of priorities, no explicit EOI
+        ;OUT  PIC_8259_LOW				;OCW2 is written to the low port of 8259
 ;        MVI  A, 4BH				    ;OCW3 - ESMM SMM RESET SPECIAL MASK, NO POLL COMMAND, RR_RIS_READ_IS_REG
 ;        OUT  PIC_8259_LOW				;OCW3 is written to the low port of 8259
         ;Initialize M6442B RTC
-        MVI  A, 04H                     ;30 AJD = 0, IRQ FLAG = 1 (required), BUSY = 0(?), HOLD = 0
-        OUT  RTC_CTRLD_REG
-        MVI  A, 06H                     ;Innterrupt mode, STD.P enabled, 1 s.
-        OUT  RTC_CTRLE_REG
-        MVI  A, 04H                     ;TEST = 0, 24h mode, STOP = 0, RESET = 0
-        OUT  RTC_CTRLF_REG
+        ;MVI  A, 04H                     ;30 AJD = 0, IRQ FLAG = 1 (required), BUSY = 0(?), HOLD = 0
+        ;OUT  RTC_CTRLD_REG
+        ;MVI  A, 06H                     ;Innterrupt mode, STD.P enabled, 1 s.
+        ;OUT  RTC_CTRLE_REG
+        ;MVI  A, 04H                     ;TEST = 0, 24h mode, STOP = 0, RESET = 0
+        ;OUT  RTC_CTRLF_REG
         		
         LXI  B, 3                       ;BYTES TO TRANSFER
         LXI  D, OUTIO_ROM               ;SOURCE
@@ -2011,16 +2011,16 @@ INIT:   STA  OCSW
         
 
 ;       Initialize keyboard
-        LXI D, KBDMSG                       ;Print KBD Init message
-        CALL PRTSTG
-        CALL KBDINIT                        ;Call init routine
-        MOV L, B                            ;Check and print result code
-        MVI H, 00H
-        MVI C, 02H
-        CALL PRTNUM
-        CALL CRLF
+        ;LXI D, KBDMSG                       ;Print KBD Init message
+        ;CALL PRTSTG
+        ;CALL KBDINIT                        ;Call init routine
+        ;MOV L, B                            ;Check and print result code
+        ;MVI H, 00H
+        ;MVI C, 02H
+        ;CALL PRTNUM
+        ;CALL CRLF
         ;Enable interrupts
-        EI
+        ;EI
         
 PATLOP:
         CALL CRLF
@@ -2049,18 +2049,18 @@ OC3:    IN   UART_8251_CTRL             ;COME HERE TO DO OUTPUT
         MVI  A,CR                       ;GET CR BACK IN A
         RET
 ;
-;CHKIO:  IN   UART_8251_CTRL             ;*** CHKIO ***
-;        NOP                             ;STATUS BIT FLIPPED?
-;        ANI  RxRDY_MASK                         ;MASK STATUS BIT
-;        RZ                              ;NOT READY, RETURN "Z"
-;        IN   UART_8251_DATA                         ;READY, READ DATA
-CHKIO:	PUSH B
-		PUSH D
-		PUSH H
-		CALL KBD2ASCII
-		POP H
-		POP D
-		POP B
+CHKIO:  IN   UART_8251_CTRL             ;*** CHKIO ***
+        NOP                             ;STATUS BIT FLIPPED?
+        ANI  RxRDY_MASK                         ;MASK STATUS BIT
+        RZ                              ;NOT READY, RETURN "Z"
+        IN   UART_8251_DATA                         ;READY, READ DATA
+;CHKIO:	PUSH B
+;		PUSH D
+;		PUSH H
+;		CALL KBD2ASCII
+;		POP H
+;		POP D
+;		POP B
 		CPI  00H
 		RZ
         ANI  7FH                        ;MASK BIT 7 OFF
@@ -2506,16 +2506,6 @@ RTC_ISR:
 		POP PSW							;Restore machine status
         EI                              ;Re-enable interrupts
 		RET								;Return to interrupted program
-		
-VDP_ISR:
-		PUSH PSW						;Save condition bits and accumulator
-        PUSH H
-        PUSH D
-        POP D
-        POP H        
-		POP PSW							;Restore machine status
-        EI                              ;Re-enable interrupts
-		RET								;Return to interrupted program		
 
 ;Interrupt vectors
 IR0_VECT:
