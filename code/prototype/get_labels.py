0c000a21411c628af50c776732b2e4bcbac42fe1
Marek Wiecek <marekw1986@gmail.com>
2024-06-04 16:55:32 +0200

    Minor changes

--- code/prototype/get_labels.py
+++ code/prototype/get_labels.py
@@ -8,7 +8,13 @@ with open('tinybasic-2.0.lst', 'r') as file:
 	lines = labels.split("\n")
 	#print(lines)
 	for line in lines:
-		x = line.split('\s{3,}')
-		print(x)
+		x = line.split()
+		odd = x[::2]
+		even = x[1::2]
+		#print(odd)
+		#print(even)
+		for label in even:
+			print(label + " EQU 0" + odd[even.index(label)] + "H")
+			#print(even.index(label))
 	#result = [x for x in re.split("\s{3,}",labels) if x]
 	#print(result)
